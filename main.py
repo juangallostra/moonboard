dfe3700d11c8f955235537e1b07d36ad48abb58f
Juan Gallostra <juangallostra@gmail.com>
2021-11-23 16:07:21 +0100

    add new entrypoint

---
+++ main.py
@@ -0,0 +1,15 @@
+from render_config import RendererConfig
+from problem_renderer import ProblemRenderer
+from moonboard import get_moonboard
+from moonboard import DefaultProblemAdapter
+import json
+
+if __name__ == "__main__":
+    # Create Renderer
+    config = RendererConfig()
+    renderer = ProblemRenderer(get_moonboard(2017), DefaultProblemAdapter(), config)
+    # Load data
+    with open('problems.json', 'r') as f:
+        problems = json.load(f)
+
+    renderer.render_problem(problems['339318'], with_info=True)
