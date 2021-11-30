6b3c02a65dad5af3aecc6522fd5702aaf6a4f374
Juan Gallostra <juangallostra@gmail.com>
2021-11-30 11:29:57 +0100

    Test full ahoughton cicle

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
+from generators.ahoughton import AhoughtonGenerator
 from render_config import RendererConfig
 from problem_renderer import ProblemRenderer
 from moonboard import get_moonboard
 from adapters.default import DefaultProblemAdapter
 from adapters.crg import CRGProblemAdapter
+from adapters.ahoughton import AhoughtonAdapter
 import json
 
 
 def main():
     # Create Renderer
     config = RendererConfig()
-    renderer = ProblemRenderer(get_moonboard(
-        2017), DefaultProblemAdapter(), config)
+    renderer = ProblemRenderer(
+        get_moonboard(2017), 
+        DefaultProblemAdapter(), 
+        config
+    )
     crg_renderer = ProblemRenderer(
-        get_moonboard(2017), CRGProblemAdapter(), config)
+        get_moonboard(2017), 
+        CRGProblemAdapter(), 
+        config
+    )
+    
+    ahoughton_renderer_2016 = ProblemRenderer(
+        get_moonboard(2016),
+        AhoughtonAdapter(),
+        config
+    )
+    ahoughton_generator_2016 = AhoughtonGenerator(year=2016)
+
+    ahoughton_renderer_2017 = ProblemRenderer(
+        get_moonboard(2017),
+        AhoughtonAdapter(),
+        config
+    )
+    ahoughton_generator_2017 = AhoughtonGenerator(year=2017)
+    
     # Load data
     with open('data/problems.json', 'r') as f:
         problems = json.load(f)
@@ -20,7 +43,14 @@ def main():
     with open('data/crg.json', 'r') as f:
         crg_problems = json.load(f)
         crg_renderer.render_problem(crg_problems['1'])
-
+    # Ahoughton generator and adapter test
+    # 2016
+    problem = ahoughton_generator_2016.generate()
+    ahoughton_renderer_2016.render_problem(problem)
+    # 2017
+    problem = ahoughton_generator_2016.generate()
+    ahoughton_renderer_2016.render_problem(problem)
+    
 
 if __name__ == "__main__":
     main()
